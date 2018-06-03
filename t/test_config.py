da54873fe2245067c642d71c6b90a0bc55d675e5
Paul Miller <paul@jettero.pl>
2018-06-03 10:48:34 -0400

    config tests

--- t/test_config.py
+++ t/test_config.py
@@ -2,8 +2,13 @@ import SplunkSuperLightForwarder
 
 def test_setup():
     sslf = SplunkSuperLightForwarder.setup()
-    assert sslf.config_file == '/etc/sslf.conf'
-    sslf = SplunkSuperLightForwarder.setup('-c', 't/sslf.conf')
-    assert sslf.config_file == 't/sslf.conf'
-    sslf = SplunkSuperLightForwarder.setup('-c t/sslf.conf'.split())
-    assert sslf.config_file == 't/sslf.conf'
+    assert sslf.config_file == SplunkSuperLightForwarder.Daemon.config_file
+
+    sslf = SplunkSuperLightForwarder.setup('-c', 't/sslf.1')
+    assert sslf.config_file == 't/sslf.1'
+
+    sslf = SplunkSuperLightForwarder.setup('-c t/sslf.2'.split())
+    assert sslf.config_file == 't/sslf.2'
+
+    sslf = SplunkSuperLightForwarder.setup(config_file='t/sslf.3')
+    assert sslf.config_file == 't/sslf.3'
