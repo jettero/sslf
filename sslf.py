83131fb0fbdd9952cb6091b1e83cbfd549fee113
Paul Miller <paul@jettero.pl>
2018-06-04 11:48:19 -0400

    use .run()

--- sslf.py
+++ sslf.py
@@ -3,8 +3,6 @@
 # NOTE: this isn't actually installed by setup.py it's just here for convenient
 #       ./sslf.py runs directly in the repo directory
 
-import SplunkSuperLightForwarder
-
 if __name__ == '__main__':
-    sslf = SplunkSuperLightForwarder.Daemon()
-    sslf.start()
+    import SplunkSuperLightForwarder
+    SplunkSuperLightForwarder.run()
