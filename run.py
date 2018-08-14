9df1e635ed59f60f8dbda00e432aa23c2941b9ee
Paul Miller <paul@jettero.pl>
2018-08-14 08:54:10 -0400

    rename and change note to self

---
+++ run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# NOTE: this isn't installed by setup.py. It exists only so the author can:
+# ./run.py --help
+
+if __name__ == '__main__':
+    import sslf
+    sslf.run()
--- sslf.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-# NOTE: this isn't actually installed by setup.py it's just here for convenient
-#       ./sslf.py runs directly in the repo directory
-
-if __name__ == '__main__':
-    import sslf
-    sslf.run()
