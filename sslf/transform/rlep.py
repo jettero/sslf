efdeb6a792c5f192d09b164ee0f8a4f951c911e4
Paul Miller <paul@jettero.pl>
2018-11-18 09:48:29 -0500

    teach parsed_time to deal with journald timestamp bigfatints

--- sslf/transform/rlep.py
+++ sslf/transform/rlep.py
@@ -1,9 +1,15 @@
+import re
 import logging
 
 log = logging.getLogger("sslf:trans:rlep")
 
 from sslf.re   import ReEngine
 from sslf.util import AttrDict, DateParser
+
+# date +\%s.\%N
+# 1542551942.943691901
+# 1542551942.943691901
+parse_ts_re = re.compile(r'\s*(?P<ts>\d{7,10})(?P<ns>\d*)\s*')
 
 class ReLineEventProcessor:
 
@@ -21,9 +27,21 @@ class ReLineEventProcessor:
     def rlep_line(self, line):
         evr = AttrDict(event=line, source=self.path, fields=self._re(line))
         ptv = evr.fields.get(self.parse_time)
+        parsed_time = None
         if ptv:
-            log.debug("parsing field=%s value=%s as a datetime", self.parse_time, ptv)
-            dp = DateParser(ptv)
-            evr['time'] = dp.tstamp
-            log.debug(" parsed time is %s", dp.fmt)
+            m = parse_ts_re.match(ptv)
+            if m:
+                gd = m.groupdict()
+                log.debug('understood %s to be a timestamp: %s', ptv, gd)
+                parsed_time = gd['ts']
+                if gd['ns']:
+                    parsed_time += '.' + gd['ns']
+            else:
+                log.debug("parsing field=%s value=%s as a datetime", self.parse_time, ptv)
+                dp = DateParser(ptv)
+                parsed_time = dp.tstamp
+                log.debug(" parsed time is %s", dp.fmt)
+        if parsed_time:
+            log.debug('setting evr.time = %s', parsed_time)
+            evr['time'] = parsed_time
         return evr
