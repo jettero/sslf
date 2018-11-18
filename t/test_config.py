5d529c3694d97f77bf60398332926e49ce6fd070
Paul Miller <paul@jettero.pl>
2018-11-18 09:07:52 -0500

    fixup some tests of blah

--- t/test_config.py
+++ t/test_config.py
@@ -9,7 +9,7 @@ log = logging.getLogger('sslf:test')
 
 sys.argv = [ sys.argv[0] ]
 
-def test_setup1(nc_config):
+def test_setup(nc_config):
     log.debug("-----=: setup()")
     sslf = nc_config() # Daemon.config_file = None; Daemon().update_config()
     assert sslf.config_file == sslf.__class__.config_file
@@ -30,3 +30,12 @@ def test_setup1(nc_config):
     log.debug("-----=: setup(config=t/sslf.3) --> fail")
     with pytest.raises(DaemonConfig, message="Expecting DaemonConfig error", match=r'.*?valid config.*'):
         sslf = nc_config(config='t/sslf.3')
+
+def test_config(nc_config):
+    sslf = nc_config(config_file='t/test2.conf')
+
+    assert sslf.hec == 'https://localhost:12345/'
+    assert sslf.token == 'test2-xx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+
+    jdc = sslf.paths.get('/journald', {})
+    assert 're_ts1:SYSLOG_PID' in jdc
