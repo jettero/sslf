8e73bfe25fd05c75ed9ec3e6458e2bf858177aa6
Paul Miller <paul@jettero.pl>
2018-12-07 09:12:30 -0500

    test hec load

--- t/test_cimport.py
+++ t/test_cimport.py
@@ -6,3 +6,8 @@ def test_cls_split():
     assert mod_cls_split('lines.Thingy', 'sslf.reader', 'Reader') == ('sslf.reader.lines', 'Thingy')
     assert mod_cls_split('hec.HEC', 'sslf.returner', 'Returner') == ('sslf.returner.hec', 'HEC')
     assert mod_cls_split('json', 'sslf.returner', 'Returner') == ('sslf.returner.json', 'Returner')
+
+def test_find_object():
+    o = find_namespaced_object('hec', 'sslf.returner', 'Returner', 'https://whatever', 'token-here')
+    from sslf.returner.hec import Returner as hr
+    assert isinstance(o, hr)
