c4f974eb4983e97a83ef5a84a593965c9ce5324c
Paul Miller <paul@jettero.pl>
2018-12-07 08:27:46 -0500

    test class splitting

---
+++ t/test_cimport.py
@@ -0,0 +1,8 @@
+
+from sslf.util.cimport import (mod_cls_split, find_namespaced_object)
+
+def test_cls_split():
+    assert mod_cls_split('lines', 'sslf.reader', 'Reader') == ('sslf.reader.lines', 'Reader')
+    assert mod_cls_split('lines.Thingy', 'sslf.reader', 'Reader') == ('sslf.reader.lines', 'Thingy')
+    assert mod_cls_split('hec.HEC', 'sslf.returner', 'Returner') == ('sslf.returner.hec', 'HEC')
+    assert mod_cls_split('json', 'sslf.returner', 'Returner') == ('sslf.returner.json', 'Returner')
