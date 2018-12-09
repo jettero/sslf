2e1939662a923bcaa2fcf2b97d54bae2e21c0e75
Paul Miller <paul@jettero.pl>
2018-12-09 11:21:07 -0500

    zip compare the actual lines with the event lines in the return file

--- t/test_daemon.py
+++ t/test_daemon.py
@@ -1,5 +1,6 @@
 
 import os
+import simplejson as json
 import sslf.daemon
 
 def test_config(jsonloop_daemon):
@@ -10,9 +11,21 @@ def test_config(jsonloop_daemon):
     assert list(jsonloop_daemon.paths)[0].split('/')[-2:] == ['t','file']
 
 def count_file_lines(fname='t/json-return.json'):
-    assert os.path.isfile(fname)
+    try:
+        with open(fname, 'r') as fh:
+            return len( list(fh.readlines()) )
+    except:
+        return 0
+
+def retrieve_file_lines(fname='t/file'):
     with open(fname, 'r') as fh:
-        return len( list(fh.readlines()) )
+        for line in fh.readlines():
+            yield line.rstrip()
+
+def retrieve_json_events(fname='t/json-return.json'):
+    for line in retrieve_file_lines(fname):
+        pline = json.loads(line)
+        yield pline['event'].rstrip()
 
 def test_step(jsonloop_daemon, thousand_line_tfile, caplog):
     jsonloop_daemon.step()
@@ -20,8 +33,7 @@ def test_step(jsonloop_daemon, thousand_line_tfile, caplog):
     assert 'aborting step early' in caplog.text
     jsonloop_daemon.step()
     assert count_file_lines() == 10
+    for line,event in zip(retrieve_file_lines(), retrieve_json_events()):
+        assert line == event
     jsonloop_daemon.step()
     assert count_file_lines() == 20
-    # TODO: when we get to the point that step(), step(), step() produces the
-    # right number of lines; we'll then have to make sure it's not skipping any
-    # either.
