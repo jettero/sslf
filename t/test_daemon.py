d51f150631f01fd1fbf51ee0b229408ff9a840b0
Paul Miller <paul@jettero.pl>
2018-12-09 12:31:48 -0500

    this test should pass if the fileline reader was really working right during aborts in the daemon

--- t/test_daemon.py
+++ t/test_daemon.py
@@ -1,7 +1,11 @@
 
 import os
 import simplejson as json
+import logging
+
 import sslf.daemon
+
+log = logging.getLogger(__name__)
 
 def test_config(jsonloop_daemon):
     # we can't use config_file='blah' cuz pytest freaks out
@@ -27,13 +31,22 @@ def retrieve_json_events(fname='t/json-return.json'):
         pline = json.loads(line)
         yield pline['event'].rstrip()
 
-def test_step(jsonloop_daemon, thousand_line_tfile, caplog):
+def test_step(jsonloop_daemon, thousand_line_tfile):
+    path_key  = list(jsonloop_daemon.paths)[0]
+    path_item = jsonloop_daemon.paths[path_key]
+    for i in range(0,100):
+        log.debug('-- jsonloop_daemon.step() i=%d', i)
+        jsonloop_daemon.step()
+        qcn = path_item.hec.q.cn
+        fcn = count_file_lines()
+        assert {'qcn': qcn, 'fcn': fcn} == {'qcn': 10, 'fcn': i*10}
+        assert path_item.reader.ready == True
+
     jsonloop_daemon.step()
-    assert list(jsonloop_daemon.paths.values())[0].hec.q.cn == 10
-    assert 'aborting step early' in caplog.text
-    jsonloop_daemon.step()
-    assert count_file_lines() == 10
-    for line,event in zip(retrieve_file_lines(), retrieve_json_events()):
-        assert line == event
-    jsonloop_daemon.step()
-    assert count_file_lines() == 20
+    for sline,revent in zip(retrieve_file_lines(), retrieve_json_events()):
+        log.debug('sline=%s == revent=%s', sline, revent)
+        assert sline == revent
+    qcn = path_item.hec.q.cn
+    fcn = count_file_lines()
+    assert {'qcn': qcn, 'fcn': fcn} == {'qcn': 0, 'fcn': 1000}
+    assert path_item.reader.ready == False
