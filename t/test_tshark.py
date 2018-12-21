3715c15dcafa11e97fe139c35ad2f6233b610e36
Paul Miller <paul@jettero.pl>
2018-12-21 10:10:51 -0500

    split up the tests

--- t/test_tshark.py
+++ t/test_tshark.py
@@ -9,8 +9,9 @@ def tshark_json():
     with open('t/tshark.json', 'r') as fh:
         yield json.load(fh)
 
-def test_cmd1(tshark_json):
-    c0 = tshark(config={
+@pytest.fixture
+def c0():
+    return tshark(config={
         'local_dst': 'ld',
         'local_src': 'ls',
         'tcp_syn':   'MAH_FLAGS',
@@ -18,13 +19,14 @@ def test_cmd1(tshark_json):
         'out_proto': 'ip tcp'
     })
 
+def test_cmd(c0):
     assert c0.cmd == [
         'tshark', '-i', 'wooterface', '-T', 'ek',
         '-f', 'MAH_FLAGS and ((ls and not ld) or (ld and not ls))',
         '-j', 'ip tcp', '-n' ]
-
     assert c0.parse_time == 'timestamp'
 
+def test_post_processing(c0, tshark_json):
     pp = c0.json_post_process(tshark_json)
     ppe = pp['event']
     tsl = tshark_json['layers']
