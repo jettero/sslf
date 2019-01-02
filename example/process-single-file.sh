57886e0d243362474f36f2195bfe7bf80eb45efc
Paul Miller <paul@jettero.pl>
2019-01-02 14:06:37 -0500

    change filename in example so I can run it directly

--- example/process-single-file.sh
+++ example/process-single-file.sh
@@ -2,7 +2,7 @@
 
 rm -rvf /tmp/sslf/meta
 
-./lrunner --path /tmp/sslf/blah:index=tmp,reader=jsonlines \
+./lrunner --path /tmp/file.json:index=tmp,reader=jsonlines \
     --opt pid_file=/tmp/sslf/pid log_file=/tmp/sslf/log disk_queue=/tmp/sslf/dq \
           meta_data_dir=/tmp/sslf/meta log_level=debug
 
