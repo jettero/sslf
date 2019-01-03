6bcb6d5ac62a014ec934e0964a0e831db7371e7c
Paul Miller <paul@jettero.pl>
2019-01-03 08:10:42 -0500

    yeah, this is nice

--- example/process-single-file.sh
+++ example/process-single-file.sh
@@ -5,8 +5,10 @@ rm -rvf /tmp/sslf/meta
 # NOTE: other settings come from /etc/sslf.conf
 
 FILE="${1:-${FILE:-/tmp/file.json}}"
+STYPE="${2:-${STYPE:-single}}"
+INDEX="${3:-${INDEX:-tmp}}"
 
-./lrunner --path "$FILE:index=tmp,reader=jsonlines" \
+./lrunner --path "$FILE:index=$INDEX,reader=jsonlines,sourcetype=$STYPE" \
     --opt pid_file=/tmp/sslf/pid log_file=/tmp/sslf/log disk_queue=/tmp/sslf/dq \
           meta_data_dir=/tmp/sslf/meta log_level=debug one_step=true
 
